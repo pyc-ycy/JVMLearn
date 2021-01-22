3b3714b4fa349d97a6e7c070c96be9eafd97a551
7wc98#14 <2923616405@qq.com>
2021-01-22 20:12:27 +0800

    commit

---
+++ src/java/lang/PYCStart.java
@@ -0,0 +1,20 @@
+package java.lang;
+
+/**
+ * @author 御承扬
+ * @product IntelliJ IDEA
+ * @project projectOne
+ * @file PYCStart
+ * @pack java.lang
+ * @date 2021/1/22
+ * @time 20:05
+ * @E-mail 2923616405@qq.com
+ **/
+
+
+public class PYCStart {
+    // 抛出错误：java.lang.SecurityException: Prohibited package name: java.lang
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
