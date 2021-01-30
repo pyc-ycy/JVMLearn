78d4a0646664e37f54df2f53ea8e2995e7e64223
7wc98#14 <2923616405@qq.com>
2021-01-30 20:31:23 +0800

    commit

---
+++ src/com/pyc/CP4/IHaveNatives.java
@@ -0,0 +1,23 @@
+package com.pyc.CP4;
+
+/**
+ * @author 御承扬
+ * @product IntelliJ IDEA
+ * @project projectOne
+ * @file IHaveNatives
+ * @pack com.pyc.CP4
+ * @date 2021/1/30
+ * @time 20:10
+ * @E-mail 2923616405@qq.com
+ **/
+
+
+public class IHaveNatives {
+    public native void Native1(int x);
+
+    native static public long Native2();
+
+    private native synchronized float Native3(Object o);
+
+    native void Native4(int[] ary) throws Exception;
+}
