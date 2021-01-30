03c5f52f663b6a8b518fbec73f9d0c67e87d9f4d
7wc98#14 <2923616405@qq.com>
2021-01-30 22:12:41 +0800

    commit

---
+++ src/com/pyc/CP4/HeapDemo.java
@@ -0,0 +1,25 @@
+package com.pyc.CP4;
+
+/**
+ * @author 御承扬
+ * @product IntelliJ IDEA
+ * @project projectOne
+ * @file HeapDemo
+ * @pack com.pyc.CP4
+ * @date 2021/1/30
+ * @time 21:58
+ * @E-mail 2923616405@qq.com
+ **/
+
+
+public class HeapDemo {
+    public static void main(String[] args) {
+        System.out.println("Start...");
+        try{
+            Thread.sleep(1000000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        System.out.println("end....");
+    }
+}
